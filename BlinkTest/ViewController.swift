0700d7b4d14a373473547ad0e9a99c2e9720001e
qingjie <qzhao02@syr.edu>
2015-06-25 10:27:20 -0400

    test json post
    api.rb is using sinatra

---
+++ BlinkTest/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  BlinkTest
+//
+//  Created by qingjiezhao on 6/24/15.
+//  Copyright (c) 2015 qingjiezhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBAction func okTapped(sender: AnyObject) {
+        // Correct url and username/password
+        self.post(["username":"qingjie", "password":"password"], url: "http://localhost:4567/login") { (succeeded: Bool, msg: String) -> () in
+            var alert = UIAlertView(title: "Success!", message: msg, delegate: nil, cancelButtonTitle: "Okay.")
+            if(succeeded) {
+                alert.title = "Success!"
+                alert.message = msg
+            }
+            else {
+                alert.title = "Failed :("
+                alert.message = msg
+            }
+            
+            // Move to the UI thread
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                // Show the alert
+                alert.show()
+            })
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func post(params : Dictionary<String, String>, url : String, postCompleted : (succeeded: Bool, msg: String) -> ()) {
+        var request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        var session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+        var accessToken = ""
+        
+        var err: NSError?
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+        
+        //request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.setValue("access_token=\(accessToken)", forHTTPHeaderField:"Authorization")
+        
+        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            println("Response: \(response)")
+            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("Body: \(strData)")
+            var err: NSError?
+            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
+            
+            var msg = "No message"
+            
+            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+            if(err != nil) {
+                println(err!.localizedDescription)
+                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("Error could not parse JSON: '\(jsonStr)'")
+                postCompleted(succeeded: false, msg: "Error")
+            }
+            else {
+                // The JSONObjectWithData constructor didn't return an error. But, we should still
+                // check and make sure that json has a value using optional binding.
+                if let parseJSON = json {
+                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                    if let success = parseJSON["success"] as? Bool {
+                        println("Succes: \(success)")
+                        postCompleted(succeeded: success, msg: "Logged in.")
+                    }
+                    return
+                }
+                else {
+                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println("Error could not parse JSON: \(jsonStr)")
+                    postCompleted(succeeded: false, msg: "Error")
+                }
+            }
+        })
+        
+        task.resume()
+    }
+
+}
+
---
+++ api.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'sinatra'
+require 'json'
+get '/' do
+  "Nothing to see here. Move along."
+end
+post '/login' do
+  values = JSON.parse(request.env["rack.input"].read)
+  values
+  if values["username"]=='qingjie' && values["password"]=='password'
+    "{\"success\":1}"
+  else
+    "{\"success\":0}"
+  end
+end
